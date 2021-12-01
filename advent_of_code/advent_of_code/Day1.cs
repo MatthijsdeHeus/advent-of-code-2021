84e5b155470ca8f78e3ef4f0e6d8bf1d030559ce
MatthijsdeHeus <matthijs.heus@gmail.com>
2021-12-01 01:11:55 +0100

    Added basic structure

---
+++ advent_of_code/advent_of_code/Day1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace advent_of_code
+{
+    static class Day1
+    {
+        static List<string> input = File.ReadAllLines(@"C:\Users\Matthijs\Documents\advent-of-code\advent-of-code-2021\advent_of_code\advent_of_code\Input\day1.txt").ToList();
+
+        public static string Run()
+        {
+            string Output = "";
+
+            return Output;
+        }
+    }
+}
---
+++ advent_of_code/advent_of_code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace advent_of_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Day1.Run());
+        }
+    }
+}
